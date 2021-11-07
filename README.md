2ac97be6a40663168ef4ed85af48b2705f8fb392
iceBear67 <icebear67@sfclub.cc>
2021-11-07 09:42:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# SpigotHere
+A simple plugin that helps your friends in LAN to connect your bukkit server.
+
+# Usage
+Download, and throw it into your `plugins` folder.  
+
+# Requirement
+
+1. Java 8+
+2. Any minecraft version higher than `1.2`
+3. In same LAN
---
+++ src/main/java/io/ib67/SpigotHere.java
@@ -0,0 +1,30 @@
+package io.ib67;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotHere extends JavaPlugin {
+    private boolean enabled;
+    private String motd;
+    @Override
+    public void onEnable() {
+        getDataFolder().mkdirs();
+        saveDefaultConfig();
+        reloadConfig();
+        motd = getConfig().getString("motd");
+        if(motd.isEmpty()){
+            motd = Bukkit.getMotd();
+        }
+        enabled = getConfig().getBoolean("enable");
+        if(!enabled){
+            getLogger().info("Not enabled from config.");
+            return;
+        }
+        if(Bukkit.getIp().equals("127.0.0.1")){
+            getLogger().warning("It seems that you've set your server's IP to 127.0.0.1, which means other people CAN'T join your server.");
+        }
+        DiscoverMe.INSTANCE.removeEntry(new ServerEntry(motd, null, Bukkit.getPort())); // for hot-reloading
+        DiscoverMe.INSTANCE.addEntry(Bukkit.getMotd(),Bukkit.getPort());
+        getLogger().info("Service is running!");
+    }
+}
