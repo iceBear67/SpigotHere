d3b24094a88cbcb8192963fea15de6fdee8276b4
iceBear67 <icebear67@sfclub.cc>
2021-11-07 09:49:02 +0800

    Fix color support and custom motd support

--- src/main/java/io/ib67/SpigotHere.java
+++ src/main/java/io/ib67/SpigotHere.java
@@ -1,6 +1,7 @@
 package io.ib67;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpigotHere extends JavaPlugin {
@@ -23,8 +24,9 @@ public class SpigotHere extends JavaPlugin {
         if(Bukkit.getIp().equals("127.0.0.1")){
             getLogger().warning("It seems that you've set your server's IP to 127.0.0.1, which means other people CAN'T join your server.");
         }
+        motd = ChatColor.translateAlternateColorCodes('&', motd);
         DiscoverMe.INSTANCE.removeEntry(new ServerEntry(motd, null, Bukkit.getPort())); // for hot-reloading
-        DiscoverMe.INSTANCE.addEntry(Bukkit.getMotd(),Bukkit.getPort());
+        DiscoverMe.INSTANCE.addEntry(motd, Bukkit.getPort());
         getLogger().info("Service is running!");
     }
 }
